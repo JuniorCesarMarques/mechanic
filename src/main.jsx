9c29e175ff9e579170f1edab564dd5da3ad9e363
JuniorCesarMarques <juniorcms1995@gmail.com>
2024-08-22 14:16:29 -0300

    Removi react router e /page funcionando localmente

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import Mechanic from './Components_mecanica/Mechanic/Mechanic';
 import Therapist from './Components_terapeuta/Therapist/Therapist';
 import './index.css';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+const path = window.location.pathname;
 
-const router = createBrowserRouter([
-  {
-    path: "/mecanica",
-    element: <Mechanic />
-  },
-  {
-    path: "/tarot",
-    element: <Therapist />
-  },
-]);
+function App() {
+  if (path === '/tarot') {
+    return <Therapist />;
+  } else if (path === '/mecanica') {
+    return <Mechanic />
+  }
+
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}>
-      <Mechanic />
-      <Therapist />
-    </RouterProvider>
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
